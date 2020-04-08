f3c7709df02dd456341a399c6391a029c9398fc4
Caligiuri Cauã dos Santos <kalikaua@gmail.com>
2020-04-07 23:10:01 -0300

    Adicionadas trativas para tela da timeline

--- src/pages/FullRequest/index.js
+++ src/pages/FullRequest/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState }from 'react';
+import { MdTimeline } from 'react-icons/md';
 
 import empty from '../../assets/animations/empty.json';
 import Animated from '../Animate/index';
+import ChartRequest from '../Chart/index';
 
 import './styles.css';
 
 export default function FullRequest({ data }){
+    const [showHistorical, setShowHistorical ] =  useState(false);
+
     return(
         <main>
             {
@@ -14,24 +18,37 @@ export default function FullRequest({ data }){
                     <Animated name={empty} width={200} height={200}/>
                   </div>
                 : <div className="full-request">
-                    <div className="header-request">
-                        <span className="request-number">{data.number}</span>
-                        <span className="request-type-description">{data.type}</span>  
-                        <span className="request-type" style={{ background: data.color }}/>
-                    </div>
-                    <div className="request-content">{data.description}</div>
-                    <div className="request-tags"> 
-                        {   
-                            data.length === 0 
-                            ? <span>#</span>
-                            : data.tags.split(',').map((tag, index) =>(
-                                <span key={index}>{tag.trim()}</span>
-                            ))
-                        }
-                    </div>
+                    {
+                        showHistorical
+                        ? <ChartRequest  />
+                        : <div>
+                            <div className="header-request">
+                                <span className="request-number">{data.number}</span>
+                                <span className="request-type-description">{data.type}</span>  
+                                <span className="request-type" style={{ background: data.color }}/>
+                            </div>
+                            <div className="request-content">{data.description}</div>
+                            <div className="request-tags"> 
+                                {   
+                                    data.length === 0 
+                                    ? <span>#</span>
+                                    : data.tags.split(',').map((tag, index) =>(
+                                        <span key={index}>{tag.trim()}</span>
+                                    ))
+                                }
+                            </div>
+                         </div>
+                    }
+                    
                     <div className="request-footer">
                         <span>{data.consultant}</span>
                         <span>{data.date}</span>
+                        <button 
+                            className="button-timeline"
+                            onClick={() => setShowHistorical(!showHistorical)}
+                        >
+                            <MdTimeline size={28} color="#696969" />
+                        </button>
                     </div>
                 </div>
             }
