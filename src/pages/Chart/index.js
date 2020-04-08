9b491a544a0bbff0c72bec2c9b68cca64a47b616
Caligiuri Cauã dos Santos <kalikaua@gmail.com>
2020-04-07 23:08:57 -0300

    Criado tilmeline com estados estágios do pedido

---
+++ src/pages/Chart/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './styles.css';
+
+export default function ChartRequest(){
+    return (
+        <div className="container">
+           <div className="timeline">
+                <div className="item left">
+                    <div className="content">
+                        <h3>Abertura do pedido</h3>
+                    </div>
+                </div>
+                <div className="item right">
+                    <div className="content">
+                        <h3>Impedido</h3>
+                        <p>Situação está com o Wagner. Foi explicaod via email.</p>
+                    </div>
+                </div>
+                <div className="item left">
+                    <div className="content">
+                        <h3>Desvalidado Desenvolvimento</h3>
+                    </div>
+                </div>
+                <div className="item right">
+                    <div className="content">
+                        <h3>Validado Desenvolvimento</h3>
+                    </div>
+                </div>
+                <div className="item left">
+                    <div className="content">
+                        <h3>Validado Analista</h3>
+                    </div>
+                </div>
+                <div className="item right">
+                    <div className="content">
+                        <h3>Validado Consultoria</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
