32f108eec9dac4bd3dc1d47b51502e5f6ec121ed
Caligiuri Cauã dos Santos <kalikaua@gmail.com>
2020-04-21 23:19:53 -0300

    New route Adicionada a rota do dashboard

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Search from './pages/Search/index'
+import Dashboard from './pages/Dashboard/index'
 
 export default function Routes(){
     return(
         <BrowserRouter>
             <Switch>
                 <Route path='/' component={Search} exact />
+                <Route path='/dashboard' component={Dashboard} exact />
             </Switch>
         </BrowserRouter>
     )
