f446d69e97796d8a3afd558af5daf2d473265ecc
Caligiuri Cauã dos Santos <kalikaua@gmail.com>
2020-04-24 00:01:27 -0300

    Changes Realizadas alterações para valores iniciais.

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,23 +2,44 @@ import React from 'react';
 import './styles.css';
 
 import CardRequestInfo from './CardRequestInfo/index';
+import CardPatchInfo from './CardPatchInfo/index';
 
 import kundenLogo from '../../assets/img/kundenLogo.png';
 
 export default function Dashboard() {
+    const data = [
+        { type:'ESPECIFICAÇÃO', qtd: 15, },
+        { type:'PROJETO', qtd: 15,},
+        { type:'INFRAESTRUTURA', qtd: 15,},
+        { type:'RECURSOS HUMANOS', qtd: 15, },
+        { type:'ERRO', qtd: 15,  },
+        { type:'ABONADO', qtd: 15, },
+    ];
+
+    const dataPatch =[
+        { type: 'patch' },
+        { type: 'up' },
+        { type: 'down' },
+    ]
     return (
         <div className="container-dashboard">
             <header>
                 <h1>Dashboard</h1>
                 <img src={kundenLogo} alt="kunden logo" />
             </header>
-            <div className="line-request-infos">
-                <CardRequestInfo />
-                <CardRequestInfo />
-                <CardRequestInfo />
-                <CardRequestInfo />
-                <CardRequestInfo />
-                <CardRequestInfo />
+            <div className="line-infos">
+                {
+                    data.map(item => (
+                        <CardRequestInfo type={item.type} qtd={item.qtd}/>           
+                    ))
+                }
+            </div>
+            <div className="line-infos">
+                {   
+                    dataPatch.map(item => (
+                        <CardPatchInfo type={item.type}/>           
+                    ))
+                }
             </div>
         </div>
     )
