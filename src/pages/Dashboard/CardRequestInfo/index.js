702bc0b409709e551336b8ab574fe3c904110756
Caligiuri Cauã dos Santos <kalikaua@gmail.com>
2020-04-25 17:56:19 -0300

    Change Alterado a funcão de render do icon, para um componente isolado.

--- src/pages/Dashboard/CardRequestInfo/index.js
+++ src/pages/Dashboard/CardRequestInfo/index.js
@@ -1,50 +1,13 @@
 import React from 'react';
 import './styles.css';
-import { MdAttachMoney } from 'react-icons/md';
-import { AiFillFileText, AiFillWarning } from 'react-icons/ai';
-import { FaToolbox } from 'react-icons/fa';
-import { BsFillPersonFill, BsClock } from 'react-icons/bs';
 
+import HandleIcon from '../HandleIcons/index';
 
 export default function CardRequestInfo({ type, qtd }) {
-    function handleIcon(){
-        switch(type){
-            case 'ERRO': return (
-                            <div className="mini-card" style={{background: '#F63030'}}>
-                                    <AiFillWarning size={50} color='#fff'/>
-                            </div>
-                        )
-            case 'RECURSOS HUMANOS': return (
-                            <div className="mini-card" style={{background: '#BCBCBC'}}>
-                                    <BsFillPersonFill size={50} color='#fff'/>
-                            </div>
-                        )
-            case 'ESPECIFICAÇÃO': return (
-                            <div className="mini-card" >
-                                    <MdAttachMoney size={50} color='#fff' />
-                            </div>
-                        )
-            case 'ABONADO': return (
-                            <div className="mini-card" style={{background: '#EE8524'}}>
-                                    <BsClock size={50} color='#fff' />
-                            </div>
-                        )
-            case 'INFRAESTRUTURA': return (
-                            <div className="mini-card" style={{background: '#FAE637'}}>
-                                    <FaToolbox size={50} color='#fff'  />
-                            </div>
-                        )
-            case 'PROJETO': return (
-                            <div className="mini-card"  style={{background: '#6FA6E7'}}>
-                                    <AiFillFileText size={50} color='#fff'/>
-                            </div>
-                        )
-            default: return null                        
-        }
-    }
+    
     return (        
        <div className="card">           
-            { handleIcon() }           
+            <HandleIcon type={type}/>          
             <span className="type">{type.toLowerCase()}</span>
             <span className="record">{qtd}</span>
        </div>
