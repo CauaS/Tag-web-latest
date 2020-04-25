abcb636bb553526677f0c846394fc47b1262aeb5
Caligiuri Cauã dos Santos <kalikaua@gmail.com>
2020-04-25 18:05:06 -0300

    Change
    Adicionado nova property size pra o icon, pra tornar ele mais flexivel.

--- src/pages/Dashboard/HandleIcons/index.js
+++ src/pages/Dashboard/HandleIcons/index.js
@@ -7,51 +7,51 @@ import { FaToolbox } from 'react-icons/fa';
 import { BsFillPersonFill, BsClock } from 'react-icons/bs';
 import { FaTag } from 'react-icons/fa';
 
-export default function handleIcon({type}){
+export default function handleIcon({type, size }){
     switch(type){
         case 'ERRO': return (
                         <div className="mini-card" style={{background: '#F63030'}}>
-                                <AiFillWarning size={38} color='#fff'/>
+                                <AiFillWarning size={size} color='#fff'/>
                         </div>
                     )
         case 'RECURSOS HUMANOS': return (
                         <div className="mini-card" style={{background: '#BCBCBC'}}>
-                                <BsFillPersonFill size={38} color='#fff'/>
+                                <BsFillPersonFill size={size} color='#fff'/>
                         </div>
                     )
         case 'ESPECIFICAÇÃO': return (
                         <div className="mini-card" >
-                                <MdAttachMoney size={38} color='#fff' />
+                                <MdAttachMoney size={size} color='#fff' />
                         </div>
                     )
         case 'ABONADO': return (
                         <div className="mini-card" style={{background: '#EE8524'}}>
-                                <BsClock size={38} color='#fff' />
+                                <BsClock size={size} color='#fff' />
                         </div>
                     )
         case 'INFRAESTRUTURA': return (
                         <div className="mini-card" style={{background: '#FAE637'}}>
-                                <FaToolbox size={38} color='#fff'  />
+                                <FaToolbox size={size} color='#fff'  />
                         </div>
                     )
         case 'PROJETO': return (
                         <div className="mini-card"  style={{background: '#6FA6E7'}}>
-                                <AiFillFileText size={38} color='#fff'/>
+                                <AiFillFileText size={size} color='#fff'/>
                         </div>
                     )
          case 'up': return (
                         <div className="mini-card" style={{background: '#71E76F'}}>
-                                <AiOutlineArrowUp size={50} color='#fff'/>
+                                <AiOutlineArrowUp size={size} color='#fff'/>
                         </div>
                     )
         case 'down': return (
                         <div className="mini-card" style={{background: '#FF9587'}}>
-                                <AiOutlineArrowDown size={50} color='#fff'/>
+                                <AiOutlineArrowDown size={size} color='#fff'/>
                         </div>
                     )
         case 'patch': return (
                     <div className="mini-card" style={{background: '#6FA6E7'}}>
-                        <FaTag size={40} color='#fff' />
+                        <FaTag size={size} color='#fff' />
                     </div> 
                 ) 
         default: return null                        
