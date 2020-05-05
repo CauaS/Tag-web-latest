67b693ec4601ca7de6ee1bfdd6e0135c503b4afe
Caligiuri Cauã dos Santos <kalikaua@gmail.com>
2020-05-04 22:47:29 -0300

    Change

--- src/pages/Dashboard/CardPatchInfo/index.js
+++ src/pages/Dashboard/CardPatchInfo/index.js
@@ -6,9 +6,9 @@ import HandleIcon from '../HandleIcons/index';
 export default function CardPatchInfo({ rate, type, qtd }) {
     return (        
        <div className="card-patch">       
-            <HandleIcon type={rate} size={40} /> 
+            <HandleIcon type={rate} size={38} /> 
             <span className="description">
-                <HandleIcon type={type} size={35} />    
+                <HandleIcon type={type} size={38} />    
             </span>    
             <span className="number-patch">{rate === 'patch' ? '213' : qtd }</span>
             <div className="footer-patch">
