cb14c9f8a49219121d8882299c76e8f6cf76f2b4
Caligiuri Cauã dos Santos <kalikaua@gmail.com>
2020-05-24 20:13:04 -0300

    Change
    Alterado o componente, transformando o mesmo funcional com o BD

--- src/pages/Scrum/Column/index.js
+++ src/pages/Scrum/Column/index.js
@@ -3,42 +3,47 @@ import './styles.css';
 
 import HandleIcon from '../../Dashboard/HandleIcons/index';
 
-export default function Card(){
-    return (
-        <div className="container-card">
-            <header className="header-card">
-                <div className="icons-left">
-                    <div className="first-icon">
+export default function Card({ data }){
+    if(data.length != 0){
+        return data.map((item, index) => (
+            <div className="container-card" key={index}>
+                <header className="header-card">
+                    <div className="icons-left">
+                        <div className="first-icon">
+                                <HandleIcon 
+                                    type={item.type} 
+                                    size={15} 
+                                    borderRadius={2}
+                                />
+                            </div>
+                        <div className="second-icon">
                             <HandleIcon 
-                                type={'ERRO'} 
+                                type={item.analysed == true ? 'ANALISADO' : null } 
                                 size={15} 
                                 borderRadius={2}
                             />
                         </div>
-                    <div className="second-icon">
-                        <HandleIcon 
-                            type={'ANALISADO'} 
-                            size={15} 
-                            borderRadius={2}
-                        />
                     </div>
-                </div>
-                <span>225549</span>
-                <div className="icons-right">
-                    <div className="third-icon">
-                        <HandleIcon 
-                            type={'EXCLAMACAO'} 
-                            size={15} 
-                            borderRadius={2}
-                        />
+                    <span>{item.number}</span>
+                    <div className="icons-right">
+                        <div className="third-icon">
+                            <HandleIcon 
+                                type={'EXCLAMACAO'} 
+                                size={15} 
+                                borderRadius={2}
+                            />
+                        </div>
                     </div>
-                </div>
-            </header>
-            <footer className="footer-card">
-                <span>22/04/2020</span>
-                <span>28/04/2020</span>
-                <span>Roberto</span>
-            </footer>
-        </div>
-    )
+                </header>
+                <footer className="footer-card">
+                <span>{item.request_date}</span>
+                <span>{item.ocor_date}</span>
+                <span>{item.consultant}</span>
+                </footer>
+            </div>
+         )) 
+
+    }else {
+       return null;
+   };
 }
