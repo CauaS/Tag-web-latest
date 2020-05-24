a89523b0f24bbb29591aaa938e5c16c869411861
Caligiuri Cauã dos Santos <kalikaua@gmail.com>
2020-05-24 20:46:09 -0300

    Change
    Retirada validação da data.length e adicionada condição para o icone.

--- src/pages/Scrum/Column/index.js
+++ src/pages/Scrum/Column/index.js
@@ -4,46 +4,41 @@ import './styles.css';
 import HandleIcon from '../../Dashboard/HandleIcons/index';
 
 export default function Card({ data }){
-    if(data.length != 0){
-        return data.map((item, index) => (
-            <div className="container-card" key={index}>
-                <header className="header-card">
-                    <div className="icons-left">
-                        <div className="first-icon">
-                                <HandleIcon 
-                                    type={item.type} 
-                                    size={15} 
-                                    borderRadius={2}
-                                />
-                            </div>
-                        <div className="second-icon">
+    return data.map((item, index) => (
+        <div className="container-card" key={index}>
+            <header className="header-card">
+                <div className="icons-left">
+                    <div className="first-icon">
                             <HandleIcon 
-                                type={item.analysed == true ? 'ANALISADO' : null } 
+                                type={item.type} 
                                 size={15} 
                                 borderRadius={2}
                             />
                         </div>
+                    <div className="second-icon">
+                        <HandleIcon 
+                            type={item.analysed == true ? 'ANALISADO' : null } 
+                            size={15} 
+                            borderRadius={2}
+                        />
                     </div>
-                    <span>{item.number}</span>
-                    <div className="icons-right">
-                        <div className="third-icon">
-                            <HandleIcon 
-                                type={'EXCLAMACAO'} 
-                                size={15} 
-                                borderRadius={2}
-                            />
-                        </div>
+                </div>
+                <span>{item.number}</span>
+                <div className="icons-right">
+                    <div className="third-icon">
+                        <HandleIcon 
+                            type={item.event_description != 'Liberado' ?'EXCLAMACAO' : null} 
+                            size={15} 
+                            borderRadius={2}
+                        />
                     </div>
-                </header>
-                <footer className="footer-card">
-                <span>{item.request_date}</span>
-                <span>{item.ocor_date}</span>
-                <span>{item.consultant}</span>
-                </footer>
-            </div>
-         )) 
-
-    }else {
-       return null;
-   };
+                </div>
+            </header>
+            <footer className="footer-card">
+            <span>{item.request_date}</span>
+            <span>{item.ocor_date}</span>
+            <span>{item.consultant}</span>
+            </footer>
+        </div>
+    )) 
 }
